70ad30f0943a9373079a5aabfcd0540e7d7f0eba
Lucas Teixeira Holanda <69094870+Teixeira-art@users.noreply.github.com>
2025-11-21 10:04:13 -0300

    Update scrambler.cpp

--- scrambler.cpp
+++ scrambler.cpp
@@ -1,39 +1,35 @@
+# GERADOR DE SCRAMBLE 3X3 RUBIK'S CUBE
 
-#GERADOR DE SCRAMBLE 3X3 RUBIKS CUBE
 import random
-#criando o vetor de strings que vai conter os movimentos possiveismovimentos = ["R ", "R'", "R2","L ", "L'", "L2","U ", "U'", "U2","B ", "B'", "B2","F ", "F'", "F2","D ", "D'", "D2"]
-def verificador_mov(lista_movimentos,atual, anterior):
-    i = 0;    while(i == 0):
-        if(lista_movimentos[atual] == lista_movimentos[anterior]):            lista_movimentos[atual] = random.choice(movimentos)
-                    break
-    
 
+movimentos = ["R", "R'", "R2","L", "L'", "L2","U", "U'", "U2","B", "B'", "B2","F", "F'", "F2","D", "D'", "D2"]
+eixo = {"R": "x", "L": "x","U": "y", "D": "y","F": "z", "B": "z"}
 
+scramble = []
 
+for i in range(20):
+    movimentos_validos = []
 
+    for m in movimentos:
+        face_atual = m[0]
 
+        if len(scramble) >= 1:
+            ultimo = scramble[len(scramble) - 1]
+            face_ultimo = ultimo[0]
+            if face_atual == face_ultimo:
+                continue 
 
-lista_movimentos = []lista_movimentos.append(random.choice(movimentos))
-#escolhe um movimento aleatorio entre os possiveis e printa ele na tela.
-for i in range(1,20):    lista_movimentos.append(random.choice(movimentos))
-    if(i == 1):         verificador_mov(lista_movimentos,i, i-1)
-    else:        if(lista_movimentos[i] == "R " or lista_movimentos[i] == "R2" or lista_movimentos[i] == "R'"):
-            if(lista_movimentos[i-1] == "L " or lista_movimentos[i-1] == "L2" or lista_movimentos[i-1] == "L'"):                verificador_mov(lista_movimentos,i, i-2)
-            else:                verificador_mov(lista_movimentos,i, i-1)
-                        elif(lista_movimentos[i] == "L " or lista_movimentos[i] == "L2" or lista_movimentos[i] == "L'"):
-            if(lista_movimentos[i-1] == "R " or lista_movimentos[i-1] == "R2" or lista_movimentos[i-1] == "R'"):                verificador_mov(lista_movimentos,i, i-2)
-            else:                verificador_mov(lista_movimentos,i, i-1)
-                        elif(lista_movimentos[i] == "U " or lista_movimentos[i] == "U2" or lista_movimentos[i] == "U'"):
-            if(lista_movimentos[i-1] == "D " or lista_movimentos[i-1] == "D2" or lista_movimentos[i-1] == "D'"):                verificador_mov(lista_movimentos,i, i-2)
-            else:                verificador_mov(lista_movimentos,i, i-1)
-                        elif(lista_movimentos[i] == "D " or lista_movimentos[i] == "D2" or lista_movimentos[i] == "D'"):
-            if(lista_movimentos[i-1] == "U " or lista_movimentos[i-1] == "U2" or lista_movimentos[i-1] == "U'"):                verificador_mov(lista_movimentos, i, i-2)
-            else:                verificador_mov(lista_movimentos,i, i-1)
-                        elif(lista_movimentos[i] == "F " or lista_movimentos[i] == "F2" or lista_movimentos[i] == "F'"):
-            if(lista_movimentos[i-1] == "B " or lista_movimentos[i-1] == "B2" or lista_movimentos[i-1] == "B'"):                verificador_mov(lista_movimentos,i, i-2)
-            else:                verificador_mov(lista_movimentos,i, i-1)
-                        elif(lista_movimentos[i] == "B " or lista_movimentos[i] == "B2" or lista_movimentos[i] == "B'"):
-            if(lista_movimentos[i-1] == "F " or lista_movimentos[i-1] == "F2" or lista_movimentos[i-1] == "F'"):                verificador_mov(lista_movimentos,i, i-2)
-            else:                verificador_mov(lista_movimentos,i, i-1)
-    for i in range(20):
-    print(lista_movimentos[i], end = " ")
+        if len(scramble) >= 2:
+            eixo_ultimo = eixo[scramble[-1][0]]
+            eixo_penultimo = eixo[scramble[-2][0]]
+            eixo_atual = eixo[face_atual]
+
+            if eixo_atual == eixo_ultimo == eixo_penultimo:
+                continue
+
+        movimentos_validos.append(m)
+
+    mov_escolhido = random.choice(movimentos_validos)
+    scramble.append(mov_escolhido)
+
+print(' '.join(scramble))
